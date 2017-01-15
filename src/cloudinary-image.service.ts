5b52a9af96437d31ea617afeb9e368b64ef87177
Alex Bainbridge <alex.bainbridge@tstc.io>
2017-01-15 11:27:00 +0000

    Fix TypeScript errors

--- src/cloudinary-image.service.ts
+++ src/cloudinary-image.service.ts
@@ -9,7 +9,7 @@ export class CloudinaryImageService {
 
     getImageUrl(publicId: string, cloudName: string, resourceType: string, type: string, transforms: CloudinaryTransforms): string {
 
-    let constuctedURL = CLOUDINARY_URL +
+    let constuctedURL : string = CLOUDINARY_URL +
       cloudName + '/' +
       resourceType + '/' +
       type + '/' +
@@ -18,7 +18,7 @@ export class CloudinaryImageService {
     // Permit format to be unset
     // If unset, Cloudinary returns the image in the format that it was uploaded in
     if (transforms.format) {
-      constuctedURL += "." + transforms.format;
+      constuctedURL += '.' + transforms.format;
     }
 
     return constuctedURL;
